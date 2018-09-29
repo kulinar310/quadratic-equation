be51e5a780c580cf0e750fb2b7675e06db1f6fcb
kulinar310 <bullet_310@mail.ru>
2018-09-29 16:13:28 +0300

    task4

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	equation = equation.replace(/\s+/g, '');
+	equation = equation.split('*x');
+	var a = +equation[0];
+	var c = +equation[2];
+
+	equation = equation[1];
+	equation = equation.split('^2');
+	var b = +equation[1];
+
+	var first = -b/ 2 /a + Math.pow(Math.pow(b,2) - 4 * a * c , 0.5) / 2 / a;
+	var second = -b/ 2 / a -Math.pow(Math.pow(b,2) - 4 * a * c ,0.5) / 2 / a;
+
+	var first = Math.round(first);
+	var second = Math.round(second);
+
+	if(first <= second){
+		return([first, second]);
+	}else{
+		return([second, first]);
+	}
 }
